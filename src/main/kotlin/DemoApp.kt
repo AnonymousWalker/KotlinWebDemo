0cf7cbd931f5a69f8726fb17def44b0deb4cd8a7
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-06-05 09:07:59 -0500

    up

---
+++ src/main/kotlin/DemoApp.kt
@@ -0,0 +1,22 @@
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Some text response from server", ContentType.Text.Html)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080, watchPaths = listOf("BlogAppKt"), module = Application::module).start()
+}
