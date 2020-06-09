0b1a785ef7affee7d3a9fcdbbe26d0ef931df2d3
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-06-09 08:01:58 -0500

    receive ajax params

--- src/main/kotlin/DemoApp.kt
+++ src/main/kotlin/DemoApp.kt
@@ -15,7 +15,6 @@ import org.wycliffeassociates.sourceaudio.upload.model.FilePathTestModel
 import org.wycliffeassociates.sourceaudio.upload.model.FileUploadModel
 import java.io.File
 
-data class Person(val name: String, val age: Int)
 
 fun Application.module() {
     install(DefaultHeaders)
@@ -27,10 +26,25 @@ fun Application.module() {
     }
     install(Routing) {
         route("/") {
-            route("get") {
-                get {
-                    call.respondText { "GET method from: ${call.request.path()}" }
-                }
+            get {
+
+//                val html = File("./src/main/kotlin/webapp/index.html").readText()
+//                call.respondText(html, ContentType.Text.Html)
+            }
+            post {
+                //val data = call.receiveParameters()
+                val data = call.receive<FilePathTestModel>()
+                val model = FilePathTestModel(
+                    fileName = "Filename",
+                    languageCode =  data.languageCode,
+                    dublinCoreId =  data.dublinCoreId,
+                    grouping =  data.grouping,
+                    mediaExtension = data.mediaExtension,
+                    mediaQuality = data.mediaQuality,
+                    expectedResult = ""
+                )
+                val result = FilePathGenerator.createPathFromFile(model.getFileUploadModel())
+                call.respondText(result)
             }
         }
     }
