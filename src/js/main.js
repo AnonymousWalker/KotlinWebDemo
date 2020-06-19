44dbf133f70ca9c176a90211f5e85856e98853b0
DJ <djpric91@gmail.com>
2020-06-19 09:17:25 -0400

    split success and error handling into functions

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ let removeOutputMessageTimeout;
 function submitForm() {
     clearTimeout(removeOutputMessageTimeout)
 
-    const fileName = document.getElementById("file").files[0].name;
+    const fileName = document.querySelector("#file").files[0].name;
     const languageCode = document.querySelector("#languageCode").value;
     const dublinCoreId = document.querySelector("#dublinCoreId").value;
     const projectId = document.querySelector("#projectId").value;
@@ -27,16 +27,24 @@ function submitForm() {
 
 function handleResponse(res) {
     if(res.data.success) {
-        document.querySelector(".main-grid__success-message").innerHTML = res.data.output
-        document.querySelector(".main-grid__error").classList.remove("main-grid__show-success-status")
-        document.querySelector(".main-grid__success").classList.add("main-grid__show-success-status")
+        showSuccess()
     } else {
-        document.querySelector(".main-grid__error-message").innerHTML = res.data.output
-        document.querySelector(".main-grid__success").classList.remove("main-grid__show-success-status")
-        document.querySelector(".main-grid__error").classList.add("main-grid__show-success-status")
+        showError()
     }
 
     removeOutputMessageTimeout = setTimeout(removeOutputMessage, timeoutDuration)
+}
+
+function showSuccess() {
+    document.querySelector(".main-grid__success-message").innerHTML = res.data.output
+    document.querySelector(".main-grid__error").classList.remove("main-grid__show-success-status")
+    document.querySelector(".main-grid__success").classList.add("main-grid__show-success-status")
+}
+
+function showError() {
+    document.querySelector(".main-grid__error-message").innerHTML = res.data.output
+    document.querySelector(".main-grid__success").classList.remove("main-grid__show-success-status")
+    document.querySelector(".main-grid__error").classList.add("main-grid__show-success-status")
 }
 
 function removeOutputMessage() {
