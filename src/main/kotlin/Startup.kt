7d5a45fb2d046dcb791d742c27e049304458515b
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-06-21 19:41:54 -0500

    move js/css files to resources package, embeded to application and serve index.html reference.
    replace "localhost:.../static/..."
    with "static/..."

--- src/main/kotlin/Startup.kt
+++ src/main/kotlin/Startup.kt
@@ -1,5 +1,13 @@
+package org.wycliffeassociates.sourceaudio.upload
+
+import com.typesafe.config.ConfigFactory
 import io.ktor.application.Application
+import io.ktor.config.ApplicationConfig
+import io.ktor.config.HoconApplicationConfig
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.loadCommonConfiguration
 import io.ktor.server.netty.Netty
 
 const val port: Int = 4567
@@ -9,6 +17,14 @@ fun main(args: Array<String>) {
         Netty,
         port = port,
         watchPaths = listOf("KotlinWeb"),
-        module = Application::module
+        module = Application::appModule
     ).start(true)
 }
+
+//fun main(args: Array<String>) {
+//    embeddedServer(Netty, configure =  {
+//
+//    }){
+//
+//    }.start(true)
+//}
